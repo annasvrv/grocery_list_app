815b65195fc5c994948494ff087ff06359cfdc7d
Anna <anna.svrva@gmail.com>
2022-12-09 17:43:52 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import "./App.css";
 function App() {
   //  after reloading keep previous list - saved
 
-  // const [items, setItems] = useState(
-  //   JSON.parse(localStorage.getItem("shoppinglist"))
-  // );
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+  // const [items, setItems] = useState([]);
 
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
